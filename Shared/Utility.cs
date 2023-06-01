4172e6f8cec027432957816cbd54467304b4ca3a
802701ad <jameswmeyer@gmail.com>
2023-06-01 11:31:14 -0700

    Add fund confirmation dialog before deletion.

--- Shared/Utility.cs
+++ Shared/Utility.cs
@@ -41,7 +41,7 @@ namespace BlazingPennies.Shared
             return sum;
         }
 
-        public static string BackendUrl(string relativeUrl, Dictionary<string, string> additional_parameters=null)
+        private static string BackendUrlDict(string relativeUrl, Dictionary<string, string> additional_parameters=null)
         {
             if (additional_parameters==null) additional_parameters = new Dictionary<string, string>();
             string root = "https://pennypincher.x10.bz/pennydev/";
@@ -58,6 +58,32 @@ namespace BlazingPennies.Shared
             builder.Query = query.ToString();
             // Return the absolute URL from the builder
             return builder.ToString();
+        }
+
+        /// <summary>
+        /// Returns a backend URL with the given relative URL and additional parameters
+        /// </summary>
+        /// <param name="relativeUrl">The relative URL to append to the root</param>
+        /// <param name="additional_parameters">An anonymous type with the additional parameters as properties</param>
+        /// <returns>A string representing the backend URL</returns>
+        public static string BackendUrl(string relativeUrl, object additional_parameters = null)
+        {
+            // If the additional_parameters is a dictionary, call the other version of the function
+            if(additional_parameters is Dictionary<string, string>)
+            {
+                return BackendUrlDict(relativeUrl, (Dictionary<string, string>)additional_parameters);
+            }
+            // Convert the anonymous type to a dictionary
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            if (additional_parameters != null)
+            {
+                foreach (var property in additional_parameters.GetType().GetProperties())
+                {
+                    dictionary[property.Name] = property.GetValue(additional_parameters).ToString();
+                }
+            }
+            // Call the first version of the function with the dictionary
+            return BackendUrl(relativeUrl, dictionary);
         }
 
         public static string FormatJson(string json)
